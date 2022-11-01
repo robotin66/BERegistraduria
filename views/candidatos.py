b470532aac9b8865b99307347f83bb6df269f4c0
Jhon Freddy Loaiza López <112454206+robotin66@users.noreply.github.com>
2022-10-31 21:22:19 -0500

    Muchas cosas mas

--- views/candidatos.py
+++ views/candidatos.py
@@ -3,6 +3,6 @@ from flask import jsonify, request, Blueprint
 candidatos_bp = Blueprint("candidatos_blue", __name__)
 
 
-@partidos_bp.route("/", methods=["GET"])
+@candidatos_bp.route("/", methods=["GET"])
 def candidatos():
     ...
